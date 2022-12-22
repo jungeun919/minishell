ed2080a112a42bc726c06b8e07cef5df8cdef086
HaiSeong <wjdgotjd9908@gmail.com>
2022-12-22 14:59:37 +0900

    lexer norm 맞추기

--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -1,58 +1,44 @@
 
 #include "../../includes/minishell.h"
 
-t_list	*lexer(char *str)
+void lexer(char *str, t_list **lexer_tokens)
 {
-	static t_list *lexer_tokens;
 	int i;
 
+	i = 0;
 	if (str[0] == '\0')
-		return lexer_tokens;
-	else if (str[0] == '|')
-	{
-		ft_lstadd_back(&lexer_tokens, ft_lstnew(ft_strdup("|")));
-		lexer(str+1);
-	}
+		return ;
 	else if (str[0] == '\"' || str[0] == '\'')
 	{
 		i = 1;
 		while (str[i] != '\0' && str[i] != str[0]) // 홀수 따옴표 갯수는 다음 단계에서 처리
 			i++;
-		ft_lstadd_back(&lexer_tokens, ft_lstnew(ft_substr(str, 0, i+1)));
-		lexer(str+i+1);
+		if (str[i] == '\0')
+			return ft_lstadd_back(lexer_tokens, ft_lstnew(ft_substr(str, 0, i+1)));
 	}
-	else if (str[0] == '<' || str[0] == '>'){
-		if (str[1] == str[0]){
-			ft_lstadd_back(&lexer_tokens, ft_lstnew(ft_substr(str, 0, 2)));
-			lexer(str+2);
-		}
-		else
-		{
-			ft_lstadd_back(&lexer_tokens, ft_lstnew(ft_substr(str, 0, 1)));
-			lexer(str+1);
-		}
-	}
+	else if ((str[0] == '<' && str[1] == '<') || (str[0] == '>' && str[1] == '>'))
+		i++;
+	else if (str[0] == '<' || str[0] == '>' || str[0] == '|')
+		;
 	else if (ft_strchr("\t\n\v\f\r ", str[0]))
-	{
-		lexer(str+1);
-	}
+		return lexer(str+1, lexer_tokens);
 	else {
-		i = 0;
-		while (!ft_strchr("|\'\"><\t\n\v\f\r ", str[i]))
+		while (!ft_strchr("|\'\"><\t\n\v\f\r ", str[i + 1]))
 			i++;
-		ft_lstadd_back(&lexer_tokens, ft_lstnew(ft_substr(str, 0, i)));
-		lexer(str+i);
 	}
-	return lexer_tokens;
-
-	// | 처리
-	// " ' 처리
-	// 		-> " or ' 이 나오면 다음 ", ' 이 나올때 까지 뒤로 간후 
-	// << >> < > 처리
+	ft_lstadd_back(lexer_tokens, ft_lstnew(ft_substr(str, 0, i+1)));
+	lexer(str+i+1, lexer_tokens);
 }
 
 int main() {
-	t_list *lexer_token = lexer("  cat <<eof >file1 && cat file1 && abc || wc <file1 | cat >file2 \"$asd 43 rew");
-	lexer_token ++;
-	lexer_token --;
+	t_list *lexer_token = NULL;
+	lexer("  cat <<eof >file1 && cat file1 && abc || wc <file1 | cat >file2 \"$asd 43 rew", &lexer_token);
+	t_list *temp = lexer_token;
+	int node_index = 0;
+	while (temp != NULL)
+	{
+		printf("%2d 번째 노드 : [%s]\n", node_index++, temp->content);
+		temp = temp->next;
+	}
+	
 }
