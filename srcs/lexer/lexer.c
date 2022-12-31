e8e28685e4e0aed71e8a81dad8b556854df58c41
HaiSeong <wjdgotjd9908@gmail.com>
2022-12-31 20:18:39 +0900

    space 만 입력했을때 에러 처리

--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: hajeong <hajeong@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 09:58:22 by hajeong           #+#    #+#             */
-/*   Updated: 2022/12/31 19:35:38 by hajeong          ###   ########.fr       */
+/*   Updated: 2022/12/31 20:18:05 by hajeong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -396,7 +396,7 @@ static void move_in_redirection(t_parser_token *parser_token)
 	t_list	*temp;
 	t_list	*prev;
 	
-	if (parser_token->cmd->label == REDIR_IN || parser_token->cmd->label == REDIR_HEREDOC) // | | 사이에 아무것도 없는 경우 없음
+	if (parser_token->cmd != NULL && (parser_token->cmd->label == REDIR_IN || parser_token->cmd->label == REDIR_HEREDOC)) // | | 사이에 아무것도 없는 경우 없음
 		move_front_in_redirection(parser_token);
 	if (parser_token->cmd == NULL)
 		return ;
@@ -434,7 +434,7 @@ static void move_out_redirection(t_parser_token *parser_token)
 	t_list	*temp;
 	t_list	*prev;
 	
-	if (parser_token->cmd->label == REDIR_OUT || parser_token->cmd->label == REDIR_DOUBLE_OUT) // | | 사이에 아무것도 없는 경우 없음
+	if (parser_token->cmd != NULL && (parser_token->cmd->label == REDIR_OUT || parser_token->cmd->label == REDIR_DOUBLE_OUT)) // | | 사이에 아무것도 없는 경우 없음
 		move_front_out_redirection(parser_token);
 	if (parser_token->cmd == NULL)
 		return ;
