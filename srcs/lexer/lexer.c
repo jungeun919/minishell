31f585b61423ee068cb6e8ed3e984a42d7db0640
HaiSeong <wjdgotjd9908@gmail.com>
2023-01-09 12:33:00 +0900

    lexer 반복문으로 refactoring

--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: hajeong <hajeong@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 09:58:22 by hajeong           #+#    #+#             */
-/*   Updated: 2023/01/02 13:25:23 by hajeong          ###   ########.fr       */
+/*   Updated: 2023/01/09 12:32:25 by hajeong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,51 @@ void	lexer(char *str, t_list **lexer_token)
 	char	*temp;
 	t_list	*new;
 
-	i = 0;
-	if (str[0] == '\0')
-		return ;
-	else if (str[0] == '\"' || str[0] == '\'')
-		while (str[++i] != '\0' && str[i] != str[0])
-			;
-	else if (ft_strncmp(str, "<<", 2) == 0 || ft_strncmp(str, ">>", 2) == 0)
-		i++;
-	else if (ft_strchr("\t\n\v\f\r ", str[0]))
-		;
-	else if (str[0] != '<' && str[0] != '>' && str[0] != '|')
-		while (!ft_strchr("|\'\"><\t\n\v\f\r ", str[i + 1]))
+	while (str[0] != '\0')
+	{
+		i = 0;
+		if (str[0] == '\"' || str[0] == '\'')
+			while (str[++i] != '\0' && str[i] != str[0])
+				;
+		else if (ft_strncmp(str, "<<", 2) == 0 || ft_strncmp(str, ">>", 2) == 0)
 			i++;
-	temp = ft_substr(str, 0, i + 1);
-	new = ft_lstnew(temp);
-	if (temp == NULL || new == NULL)
-		free_temp_clear_and_exit(lexer_token, temp);
-	ft_lstadd_back(lexer_token, new);
-	if (str[i] == '\0')
-		return ;
-	lexer(str + i + 1, lexer_token);
+		else if (ft_strchr("\t\n\v\f\r ", str[0]))
+			;
+		else if (str[0] != '<' && str[0] != '>' && str[0] != '|')
+			while (!ft_strchr("|\'\"><\t\n\v\f\r ", str[i + 1]))
+				i++;
+		temp = ft_substr(str, 0, i + 1);
+		new = ft_lstnew(temp);
+		if (temp == NULL || new == NULL)
+			free_temp_clear_and_exit(lexer_token, temp);
+		ft_lstadd_back(lexer_token, new);
+		str += i + 1;
+	}
 }
+
+// void	lexer(char *str, t_list **lexer_token)
+// {
+// 	int		i;
+// 	char	*temp;
+// 	t_list	*new;
+
+// 	i = 0;// 	else if (str[0] == '\"' || str[0] == '\'')
+// 		while (str[++i] != '\0' && str[i] != str[0])
+// 			;
+// 	else if (ft_strncmp(str, "<<", 2) == 0 || ft_strncmp(str, ">>", 2) == 0)
+// 		i++;
+// 	else if (ft_strchr("\t\n\v\f\r ", str[0]))
+// 		;
+// 	else if (str[0] != '<' && str[0] != '>' && str[0] != '|')
+// 		while (!ft_strchr("|\'\"><\t\n\v\f\r ", str[i + 1]))
+// 			i++;
+// 	temp = ft_substr(str, 0, i + 1);
+// 	new = ft_lstnew(temp);
+// 	if (temp == NULL || new == NULL)
+// 		free_temp_clear_and_exit(lexer_token, temp);
+// 	ft_lstadd_back(lexer_token, new);
+// 	if (str[i] == '\0')
+// 		return ;
+// 	lexer(str + i + 1, lexer_token);
+// }
+
